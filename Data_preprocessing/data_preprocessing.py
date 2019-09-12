60c5cefa1d1a81636d336f5eb8b8bcc3f0419cfd
AvisChiu <32427817+AvisChiu@users.noreply.github.com>
2019-09-12 13:54:32 +0800

    Update data_preprocessing.py

--- Data_preprocessing/data_preprocessing.py
+++ Data_preprocessing/data_preprocessing.py
@@ -16,7 +16,7 @@ imputer = Imputer(missing_values = "NaN", strategy = "most_frequent", axis = 0)
 imputer = imputer.fit(X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
 
-print(X)
+
 
 
 # 基於某個屬性來設施 標籤
@@ -24,8 +24,6 @@ labelencoder_X = LabelEncoder()
 X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0])
 
 
-print(X)
-print("===========")
 
 
 # 根據剛剛建立的標籤進行編碼
@@ -39,34 +37,20 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y =  labelencoder_Y.fit_transform(Y)
 
-print(X)
-print(Y)
-print(pd.DataFrame(X))
-print("==============")
 
 # 分割訓練集和測試集
 X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
 
-print(X_train)
-print(X_test)
-print(Y_train)
-print(Y_test)
-print("==============")
-
 
 # 去均值和方差归一化。且是针对每一个特征维度来做的，而不是针对样本
-# 各列的 mean 為 0 , 方差為 1
+
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.fit_transform(X_test)
 
-print(X_train)
-print(X_test)
-
-
+# 各列的 mean 為 0 , 方差為 1
 mean = X_train.mean(axis=0)
 std = X_train.std(axis=0)
 
-print(mean)
-print(std)
+
 
